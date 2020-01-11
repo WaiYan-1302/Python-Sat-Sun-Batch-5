0b84ce30b2d3ea024718433c3112a3fc2a86e3ed
WaiYan-1302 <57553357+WaiYan-1302@users.noreply.github.com>
2020-01-11 16:18:05 +0630

    Python commit

---
+++ #results.py
@@ -0,0 +1,14 @@
+x = int(input("Examination Results :"))
+
+if x>=90 & x<= 100:
+	print("A")
+elif x>= 70 & x <= 89:
+	print("B")
+elif x >= 60 & x <= 69:
+	print("C")
+elif x >= 40 & x <= 59:
+	print("D")
+elif c >= 30 & x <= 10:
+	print("Fail")
+else:
+	print("Error Input")
---
+++ If_Else_Statement.py
@@ -0,0 +1,58 @@
+print(20 > 10)
+print(20 == 10)
+print( 20 < 10)
+print(bool("Hello World"))
+print(bool(20))
+
+
+
+Pyhton Conditions
+
+Equals                      -> x  ==  y
+Not Equals           		-> x  !=  y
+Less Than           	    -> x  <   y
+Less than or equal to       -> x  <=  y
+Greater than                -> x  >   y
+Greater than or equal to    -> x  >=  y
+Boolean OR                  -> x  or  y , x | y
+Boolean AND                 -> x  and y , x & y
+Boolean NOT                 ->  not X    
+
+
+#If Statement
+
+x = 70
+y = 60
+
+if x > y:                               #Condition
+	print("x is greater than y ")
+
+
+#Elif 
+
+
+#Checking conditions
+
+x = 70
+y = 70
+
+if x > y :
+	print("x is greater than y")
+elif x == y:
+	print("x and y are equal")
+else :
+	print("x is less than y")
+
+
+x = 50
+y = 150
+
+if x > y :
+	print("x is greater than y")
+elif x == y:
+	print("x and y are equal")
+else :
+	print("x is less than y")
+
+
+
