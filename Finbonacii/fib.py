ec46d35eabc23130732c3548c84c2a5eca0c978c
WaiYan-1302 <57553357+WaiYan-1302@users.noreply.github.com>
2020-01-17 20:32:09 +0630

    FIB COMIT

---
+++ Finbonacii/fib.py
@@ -0,0 +1,31 @@
+#import fibonacii 
+
+#fibonacii.fib(100)
+# import fibonacii
+
+# f = fibonacii.fibo 
+# f()
+# f(100)
+
+# fibonacii.fibo(10000)
+
+# from fibonacii import fib, fib2
+
+# fib2(500)
+
+from fibonacii import *
+fib(500)
+print(fib2(200))
+
+# import fiboncii as fi 
+# fi.fib(500)
+
+# from fibonacii import fib as fibonacii
+# fiboncii(500)
+
+
+#https://docs.python.org/3/tutorial/modules.html#pa
+
+#>> Student (Import Folder )
+
+
